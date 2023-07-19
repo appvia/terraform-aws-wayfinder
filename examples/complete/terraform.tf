5db10861c53f6caa1167af586bae6bf406e294ce
Kashif Saadat <kashifsaadat@gmail.com>
2023-07-19 17:40:35 +0100

    Update example

--- examples/complete/terraform.tf
+++ examples/complete/terraform.tf
@@ -1,22 +1,24 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "s3" {}
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.62"
+      version = "~> 5.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.9.0"
+      version = "~> 2.9.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
+      version = "~> 1.14.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.19.0"
+      version = "~> 2.19.0"
     }
   }
 }
