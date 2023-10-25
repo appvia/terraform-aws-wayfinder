408b39f70437aff323c2d04d2ccfa910da269a2c
Mark Hughes <mark.hughes@appvia.io>
2023-10-26 00:03:55 +0100

    Bump default version to v2.4.0

--- variables.tf
+++ variables.tf
@@ -196,7 +196,7 @@ variable "wayfinder_release_channel" {
 variable "wayfinder_version" {
   description = "The version to use for Wayfinder."
   type        = string
-  default     = "v2.3.4"
+  default     = "v2.4.0"
 }
 
 variable "aws_ebs_csi_driver_addon_version" {
