d6f3aec2f82017ea5d8de4f37b589493a740e05c
Kashif Saadat <kashifsaadat@gmail.com>
2023-10-25 10:49:46 +0100

    Remove unneeded locals

--- modules/cloudaccess/main.tf
+++ modules/cloudaccess/main.tf
@@ -1,10 +1,4 @@
 locals {
-  resource_suffix = var.resource_suffix != "" ? "-${var.resource_suffix}" : ""
-
-  # an AWS IAM OIDC "provider" is NOT required when the trust is to google, nor when the user
-  # tells us they're managing it themselves, only when provisioning for an Azure client + tenant ID
-  create_azure_trust  = var.wayfinder_identity_azure_client_id != "" && var.wayfinder_identity_azure_tenant_id != "" ? true : false
-  create_google_trust = var.wayfinder_identity_gcp_service_account != "" ? true : false
-
-  azure_oidc_issuer = local.create_azure_trust ? "https://sts.windows.net/${var.wayfinder_identity_azure_tenant_id}/" : ""
+  resource_suffix   = var.resource_suffix != "" ? "-${var.resource_suffix}" : ""
+  azure_oidc_issuer = "https://sts.windows.net/${var.wayfinder_identity_azure_tenant_id}/"
 }
