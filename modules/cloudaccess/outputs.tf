9c1f128239c7cb8235c753490ca271046793de60
Mark Hughes <mark.hughes@appvia.io>
2023-10-26 09:47:01 +0100

    Sort outputs and provider requirements so they meet lint requirements

--- modules/cloudaccess/outputs.tf
+++ modules/cloudaccess/outputs.tf
@@ -1,19 +1,35 @@
 output "cluster_manager_role_arn" {
   description = "ARN of Cluster Manager IAM role to use as spec.permissions[].awsRole on the ClusterManager permission of your cloud access config"
-  value       = join("", concat(module.iam_role_cluster_manager.*.iam_role_arn, module.iam_role_cluster_manager_azure_oidc.*.iam_role_arn, module.iam_role_cluster_manager_google_oidc.*.arn))
+  value = [
+    var.from_aws ? module.iam_role_cluster_manager[0].iam_role_arn : null,
+    var.from_azure ? module.iam_role_cluster_manager_azure_oidc[0].iam_role_arn : null,
+    var.from_gcp ? module.iam_role_cluster_manager_google_oidc[0].arn : null,
+  ]
 }
 
 output "dns_zone_manager_role_arn" {
   description = "ARN of DNS Zone Manager IAM role to use as spec.permissions[].awsRole on the DNSZoneManager permission of your cloud access config"
-  value       = join("", concat(module.iam_role_dns_zone_manager.*.iam_role_arn, module.iam_role_dns_zone_manager_azure_oidc.*.iam_role_arn, module.iam_role_dns_zone_manager_google_oidc.*.arn))
+  value = [
+    var.from_aws ? module.iam_role_dns_zone_manager[0].iam_role_arn : null,
+    var.from_azure ? module.iam_role_dns_zone_manager_azure_oidc[0].iam_role_arn : null,
+    var.from_gcp ? module.iam_role_dns_zone_manager_google_oidc[0].arn : null,
+  ]
 }
 
 output "network_manager_role_arn" {
   description = "ARN of Network Manager IAM role to use as spec.permissions[].awsRole on the NetworkManager permission of your cloud access config"
-  value       = join("", concat(module.iam_role_network_manager.*.iam_role_arn, module.iam_role_network_manager_azure_oidc.*.iam_role_arn, module.iam_role_network_manager_google_oidc.*.arn))
+  value = [
+    var.from_aws ? module.iam_role_network_manager[0].iam_role_arn : null,
+    var.from_azure ? module.iam_role_network_manager_azure_oidc[0].iam_role_arn : null,
+    var.from_gcp ? module.iam_role_network_manager_google_oidc[0].arn : null,
+  ]
 }
 
 output "cloud_info_role_arn" {
   description = "ARN of Cloud Info IAM role to use as spec.permissions[].awsRole on the CloudInfo permission of your cloud access config"
-  value       = join("", concat(module.iam_role_cloud_info.*.iam_role_arn, module.iam_role_cloud_info_azure_oidc.*.iam_role_arn, module.iam_role_cloud_info_google_oidc.*.arn))
+  value = [
+    var.from_aws ? module.iam_role_cloud_info[0].iam_role_arn : null,
+    var.from_azure ? module.iam_role_cloud_info_azure_oidc[0].iam_role_arn : null,
+    var.from_gcp ? module.iam_role_cloud_info_google_oidc[0].arn : null,
+  ]
 }
