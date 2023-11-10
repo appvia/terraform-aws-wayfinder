364fa9cbce7341bccf499997b96b8eebb40cbf61
Mark Hughes <mark.hughes@appvia.io>
2023-11-10 10:10:54 +0000

    Bump Wayfinder to v2.4.3, update cloudaccess example with peering acceptor

--- modules/cloudaccess/examples/complete/outputs.tf
+++ modules/cloudaccess/examples/complete/outputs.tf
@@ -1,15 +1,3 @@
-output "cluster_manager_role_arn" {
-  value = module.wayfinder_cloudaccess.cluster_manager_role_arn
-}
-
-output "dns_zone_manager_role_arn" {
-  value = module.wayfinder_cloudaccess.dns_zone_manager_role_arn
-}
-
-output "network_manager_role_arn" {
-  value = module.wayfinder_cloudaccess.network_manager_role_arn
-}
-
-output "cloud_info_role_arn" {
-  value = module.wayfinder_cloudaccess.cloud_info_role_arn
+output "cloudaccess" {
+  value = module.wayfinder_cloudaccess
 }
