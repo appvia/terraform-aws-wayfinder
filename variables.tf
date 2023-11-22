79f4d3930123a1bdcbc6410b6a72dd483db97fae
Mark Hughes <mrsheepuk@users.noreply.github.com>
2023-11-22 17:56:19 +0000

    Bump Wayfinder to v2.4.5 (#43)
    * Bump to v2.4.5

--- variables.tf
+++ variables.tf
@@ -208,7 +208,7 @@ variable "wayfinder_release_channel" {
 variable "wayfinder_version" {
   description = "The version to use for Wayfinder."
   type        = string
-  default     = "v2.4.2"
+  default     = "v2.4.5"
 }
 
 variable "aws_ebs_csi_driver_addon_version" {
