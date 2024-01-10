aca3587fcfe1283b53f87728b4481ae54f1a0202
Kashif Saadat <kashifsaadat@gmail.com>
2024-01-10 22:45:23 +0000

    Wayfinder v2.5.1 (#50)
    * Wayfinder v2.5.1 and EKS v1.27
    
    * Bump EKS addon versions

--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ variable "cluster_security_group_additional_rules" {
 variable "cluster_version" {
   description = "The Kubernetes version to use for the EKS cluster."
   type        = string
-  default     = "1.26"
+  default     = "1.27"
 }
 
 variable "create_localadmin_user" {
@@ -208,29 +208,29 @@ variable "wayfinder_release_channel" {
 variable "wayfinder_version" {
   description = "The version to use for Wayfinder."
   type        = string
-  default     = "v2.4.6"
+  default     = "v2.5.1"
 }
 
 variable "aws_ebs_csi_driver_addon_version" {
   description = "The version to use for the AWS EBS CSI driver."
   type        = string
-  default     = "v1.20.0-eksbuild.1"
+  default     = "v1.21.0-eksbuild.1"
 }
 
 variable "coredns_addon_version" {
   description = "CoreDNS Addon version to use."
   type        = string
-  default     = "v1.9.3-eksbuild.9"
+  default     = "v1.10.1-eksbuild.6"
 }
 
 variable "kube_proxy_addon_version" {
   description = "Kube Proxy Addon version to use."
   type        = string
-  default     = "v1.26.9-eksbuild.2"
+  default     = "v1.27.8-eksbuild.4"
 }
 
 variable "aws_vpc_cni_addon_version" {
   description = "AWS VPC CNI Addon version to use."
   type        = string
-  default     = "v1.13.4-eksbuild.1"
+  default     = "v1.14.1-eksbuild.1"
 }
