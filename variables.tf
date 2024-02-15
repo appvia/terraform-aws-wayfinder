6260dd8d4d1ac0da127bf0b00ad5a345bd88866e
Kashif Saadat <kashifsaadat@gmail.com>
2024-02-15 14:10:00 +0000

    Wayfinder v2.6.5
    * Wayfinder v2.6.0 and EKS v1.28
    
    * External module updates
    
    * Add support for EKS access entries
    
    * Update examples with access entries usage
    
    * Bump Wayfinder release to v2.6.1
    
    * Update Wayfinder to v2.6.2

--- variables.tf
+++ variables.tf
@@ -221,7 +221,7 @@ variable "wayfinder_release_channel" {
 variable "wayfinder_version" {
   description = "The version to use for Wayfinder."
   type        = string
-  default     = "v2.6.4"
+  default     = "v2.6.5"
 }
 
 variable "aws_ebs_csi_driver_addon_version" {
