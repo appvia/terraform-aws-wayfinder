5a100e9b871eb99084035441c63ef03ea9d819b1
Kashif Saadat <kashifsaadat@gmail.com>
2024-02-21 10:36:01 +0000

    Wayfinder v2.6.3

--- variables.tf
+++ variables.tf
@@ -221,7 +221,7 @@ variable "wayfinder_release_channel" {
 variable "wayfinder_version" {
   description = "The version to use for Wayfinder."
   type        = string
-  default     = "v2.6.2"
+  default     = "v2.6.3"
 }
 
 variable "aws_ebs_csi_driver_addon_version" {
