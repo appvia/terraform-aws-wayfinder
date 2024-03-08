9fec7c2701cfe69c10e43a4375141bce072ff33c
Kashif Saadat <kashifsaadat@gmail.com>
2024-03-08 17:34:03 +0000

    Fix for EBS KMS Key administrators

--- eks.tf
+++ eks.tf
@@ -184,7 +184,7 @@ module "ebs_kms_key" {
 
   aliases            = ["eks/${local.name}/ebs"]
   description        = "Customer managed key to encrypt EKS managed node group volumes"
-  key_administrators = [data.aws_caller_identity.current.arn]
+  key_administrators = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
   key_service_roles_for_autoscaling = [
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/aws-service-role/autoscaling.amazonaws.com/AWSServiceRoleForAutoScaling",
     module.eks.cluster_iam_role_arn,
