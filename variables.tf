4d1c95c0ff73e76f262827196938396d30c091ea
Kashif Saadat <kashifsaadat@gmail.com>
2024-08-30 13:40:22 +0100

    feat: update Wayfinder to v2.8.6

--- variables.tf
+++ variables.tf
@@ -239,7 +239,7 @@ variable "wayfinder_release_channel" {
 variable "wayfinder_version" {
   description = "The version to use for Wayfinder."
   type        = string
-  default     = "v2.8.4"
+  default     = "v2.8.6"
 }
 
 variable "aws_ebs_csi_driver_addon_version" {
